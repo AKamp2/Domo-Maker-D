4de2ef848ba5f519813ee9aa70e70b7fbdc27ef4
Aidan Kamp <ackamp2@gmail.com>
2024-11-18 01:06:42 -0500

    domo d debugged

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -4,15 +4,13 @@ const { Account } = models;
 
 const loginPage = (req, res) => res.render('login');
 
-const signupPage = (req, res) => res.render('signup');
-
 const logout = (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
-}
+  req.session.destroy();
+  res.redirect('/');
+};
 
 const login = (req, res) => {
-  const username = `${req.body.uesrname}`;
+  const username = `${req.body.username}`;
   const pass = `${req.body.pass}`;
 
   if (!username || !pass) {
@@ -60,7 +58,6 @@ const signup = async (req, res) => {
 
 module.exports = {
   loginPage,
-  signupPage,
   login,
   logout,
   signup,
